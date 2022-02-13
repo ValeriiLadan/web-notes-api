3170b8f8497a908229c48f10c02f3515a61bbf41
Valerii Ladan <valerii.ladan@gmail.com>
2022-02-13 03:48:01 +0200

    Add NewApi project

--- CDC.WebNotes.Api/Controllers/MainController.cs
+++ CDC.WebNotes.Api/Controllers/MainController.cs
@@ -1,25 +1,17 @@
-﻿using CDC.WebNotes.Application.Contracts;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace WebNotes.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class MainController : ControllerBase
+    public class TestController : ControllerBase
     {
-        private readonly INoteService _noteService;
-
-        public MainController(INoteService noteService) {
-            _noteService = noteService;
-        }
 
         [HttpGet]
         public async Task<IActionResult> Get() {
 
-            var result = await _noteService.GetAll();
-
-            return Ok(result);
+            return Ok("!!! test !!!");
         }
     }
 }
---
+++ CDC.WebNotes.NewApi/Controllers/MainController.cs
@@ -0,0 +1,25 @@
+﻿using CDC.WebNotes.Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebNotes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+
+        public MainController(INoteService noteService) {
+            _noteService = noteService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get() {
+
+            var result = await _noteService.GetAll();
+
+            return Ok(result);
+        }
+    }
+}
